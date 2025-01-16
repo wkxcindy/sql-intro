9cd0406d270b8226a183f6bd06d3c6c816fb385c
Cindy Wang <cindy.wang1@kellogg.northwestern.edu>
2025-01-16 01:48:50 +0000

    change 2

--- lab-2-2.sql
+++ lab-2-2.sql
@@ -1,6 +1,8 @@
 -- What are the teams (and years) that have played at 
 -- Guaranteed Rate Field?
-
+SELECT year,name
+FROM teams
+WHERE park ="Guaranteed Rate Field"
 -- Expected result:
 --
 -- +------+-------------------+
--- lab-2-3.sql
+++ lab-2-3.sql
@@ -1,5 +1,9 @@
 -- What was the last team (and year) to play at U.S. Cellular Field?
-
+SELECT year, name
+FROM teams
+WHERE park ="U.S. Cellular Field"
+ORDER BY year DESC
+limit 1
 -- Expected result:
 --
 -- +------+-------------------+
